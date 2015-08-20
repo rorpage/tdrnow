a65ec2a4d8bae4fea36e09d1df145cebe98f8d10
Chris Nilghe <chris.nilghe@t-mark.co.jp>
2015-08-20 18:23:52 +0900

    Playing with ES6 https://babeljs.io/docs/learn-es2015/#ecmascript-6-features

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,18 @@
 require('./react/ui/landing-page');
 require('./plugins.js');
 
+
+/**
+ * TDR Today - Your Guide to Tokyo Disney Resort
+ *
+ * Author: Chris Nilghe
+ * Email: chris@nilghe.com
+ * 
+ * https://babeljs.io/docs/learn-es2015/#ecmascript-6-features
+ */
 $(function(){
 	'use strict';
-	console.log('A Year of Wishes!');
+	let welcomeMsg = 'A Year of Wishes!';
+	let fullMsg = `${welcomeMsg} Only at Tokyo DisneySea!`;
+	console.log(fullMsg);
 });
