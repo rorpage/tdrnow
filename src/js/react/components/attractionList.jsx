7334cc7e62cd11af5ae80265db1844abb5ccda76
Chris Nilghe <chris.nilghe@t-mark.co.jp>
2015-09-25 18:25:35 +0900

    Adding lodash dependency
      Printing out the attractions based on their location

--- src/js/react/components/attractionList.jsx
+++ src/js/react/components/attractionList.jsx
@@ -1,14 +1,31 @@
 var React = require('react');
+var _     = require('lodash');
 
 var AttractionList = React.createClass({
     render: function() {
+
+        var grouped = _.groupBy(this.props.times, 'Location');
+        var waitTimes = _.sortBy(this.props.times, 'Location');
+
+        // console.log(grouped);
+        // console.log(waitTimes);
+
+        // TODO Group all the items based on their location. So we need to get the key
+        // which is done already with the groupBy function. Now we need to iterate through.
+        // This is not right, I just threw it together
+        for (var i = 0; i < grouped.length; i++) {
+            for (var j = 0; j < grouped[i].length; j++) {
+                console.log(grouped[i][j]);
+            }
+        }
+
         return (
             <div>
                 <h4>{this.props.park}</h4>
                 
                 {this.props.error}
 
-                {this.props.times.map((info) => {
+                {waitTimes.map((info) => {
                     var fastPass = "Available";
                         if (!info.fastPass) {
                             fastPass = "Finished";
